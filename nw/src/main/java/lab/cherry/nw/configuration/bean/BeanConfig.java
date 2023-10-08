4976ee7aa53c01d6ba266b422cb8c47ddb8e82f3
taking <taking@duck.com>
2023-10-08 08:27:24 +0000

    Fix: CORS PATCH 추가

--- nw/src/main/java/lab/cherry/nw/configuration/bean/BeanConfig.java
+++ nw/src/main/java/lab/cherry/nw/configuration/bean/BeanConfig.java
@@ -65,7 +65,7 @@ public class BeanConfig {
         config.setAllowCredentials(true); // 자격 증명 허용 설정
         config.addAllowedOriginPattern("*"); // 허용할 오리진 설정
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
         source.registerCorsConfiguration("/api/**", config);
 
         return source;
