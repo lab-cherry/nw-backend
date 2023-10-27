c08e620272d2edaeace819ea994021dc558791ef
yby654 <yby654321@gmail.com>
2023-10-27 13:37:34 +0000

    test

--- nw/src/main/java/lab/cherry/nw/repository/BoardRepository.java
+++ nw/src/main/java/lab/cherry/nw/repository/BoardRepository.java
@@ -1,11 +1,9 @@
 package lab.cherry.nw.repository;
-
 import lab.cherry.nw.model.BoardEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-
 import java.util.Optional;
 import java.util.UUID;
 
