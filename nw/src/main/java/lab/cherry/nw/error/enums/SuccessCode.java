7197d3a6ed995618bc99069de130a964ba7a4a0c
taking <taking@duck.com>
2023-10-30 05:52:17 +0000

    Fix: SuccessCode 수정

--- nw/src/main/java/lab/cherry/nw/error/enums/SuccessCode.java
+++ nw/src/main/java/lab/cherry/nw/error/enums/SuccessCode.java
@@ -23,6 +23,7 @@ public enum SuccessCode {
     REGISTER_SUCCESS(200, "회원가입 완료"),
     USERID_CHECK_OK(200, "사용 가능합니다."),
     PASSWORD_RESET_OK(200, "비밀번호 초기화 이메일 발송 완료"),
+    EMAIL_INVITE_USER_OK(200, "사용자(랑부) 초대 이메일 발송 완료"),
     EMAIL_CHECK_OK(200, "이메일 인증 완료"),
     EMAIL_RESEND_OK(200, "이메일 인증 메일 재발송 완료");
 
